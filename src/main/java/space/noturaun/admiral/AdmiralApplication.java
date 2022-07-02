2895820882634a35068bade0af2528e70da32d13
Muhammad Syahrul <noturaun@gmail.com>
2022-07-03 01:01:12 +0700

    init

---
+++ src/main/java/space/noturaun/admiral/AdmiralApplication.java
@@ -0,0 +1,13 @@
+package space.noturaun.admiral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmiralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmiralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/space/noturaun/admiral/AdmiralApplicationTest.java
@@ -0,0 +1,13 @@
+package space.noturaun.admiral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmiralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
